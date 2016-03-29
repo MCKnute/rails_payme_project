62bf5c7a21cb61c7ec55cb8253c7eecb91c1b303
gkwong85 <gkwong85@hotmail.com>
2016-03-29 10:40:00 -0700

    Fixed dashboards controller queries

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,8 @@
 class DashboardsController < ApplicationController
 
 	def index
-		@clients = Client.where(company: Company.find(1))
-		@unpaidvoices = Invoice.where(company: Company.find(1), paid_date: nil)
-		@paidvoices = Invoice.where(company: Company.find(1)).where.not(paid_date: nil)
+		@clients = Client.where(company: Company.find(session[:company_id]))
+		@unpaidvoices = Invoice.where(company: Company.find(session[:company_id]), paid_date: nil)
+		@paidvoices = Invoice.where(company: Company.find(session[:company_id])).where.not(paid_date: nil)
 	end
 end
