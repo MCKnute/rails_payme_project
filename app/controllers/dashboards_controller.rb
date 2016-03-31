ba0a8f12bcaac4406c483577576591271f3c85ac
Linux Ubuntu <mcknute@gmail.com>
2016-03-31 13:02:09 -0700

    Company dashboard styling and sorting:

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,7 @@ class DashboardsController < ApplicationController
 	def index
 		@company = Company.find(session[:company_id])
 		@clients = Client.where(company: Company.find(session[:company_id]))
-		@unpaidvoices = Invoice.where(company: Company.find(session[:company_id]), paid_date: nil)
-		@paidvoices = Invoice.where(company: Company.find(session[:company_id])).where.not(:paid_date => nil)
+		@unpaidvoices = Invoice.where(company: Company.find(session[:company_id]), paid_date: nil).order(:due_by)
+		@paidvoices = Invoice.where(company: Company.find(session[:company_id])).where.not(:paid_date => nil).order(:paid_date)
 	end
 end
